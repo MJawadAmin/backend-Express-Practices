5284749c3cdbea8193ff48e5ecd51ce73f0d97f9
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-01-18 13:04:25 +0500

    added Error handlings and static files

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require ('express');
 const app = express();
 
 app.set("view engine" , "ejs");
-
+app.use(express.static('./public'));
 
 app.get('/', (req, res)=>{
     res.render('index')
+})
+
+app.get('/error', (req, res , next )=>{
+  throw Error("Somthing went wrong")
 })
 app.get('/about', (req, res)=>{
     res.render('about')
@@ -14,6 +18,14 @@ app.get('/contact', (req, res)=>{
     res.render('contact')
 })
 
+
+app.use(function errorHandler (err, req, res, next) {
+    if (res.headersSent) {
+      return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+  })
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
