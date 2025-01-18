96be1d29bc54c990036a02006334f71d583bb3e5
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-01-18 11:14:53 +0500

    commits added

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.set("view engine" , "ejs");
 app.get('/', (req, res)=>{
     res.render('index')
 })
+app.get('/about', (req, res)=>{
+    res.render('about')
+})
+app.get('/contact', (req, res)=>{
+    res.render('contact')
+})
 
 
 app.listen(3000, ()=>{
